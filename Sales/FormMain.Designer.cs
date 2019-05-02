bf4792eab59f8ee443c17417cca4f679c2679f44
SerikRakhimov <s.rakhimov@asr.rokos.local>
2019-05-02 13:17:40 +0600

    first commit

---
+++ Sales/FormMain.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Sales
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewData = new System.Windows.Forms.DataGridView();
+            this.comboBoxTable = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewData
+            // 
+            this.dataGridViewData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewData.Location = new System.Drawing.Point(22, 78);
+            this.dataGridViewData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridViewData.Name = "dataGridViewData";
+            this.dataGridViewData.RowTemplate.Height = 24;
+            this.dataGridViewData.Size = new System.Drawing.Size(773, 338);
+            this.dataGridViewData.TabIndex = 0;
+            // 
+            // comboBoxTable
+            // 
+            this.comboBoxTable.FormattingEnabled = true;
+            this.comboBoxTable.Location = new System.Drawing.Point(251, 22);
+            this.comboBoxTable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxTable.Name = "comboBoxTable";
+            this.comboBoxTable.Size = new System.Drawing.Size(250, 21);
+            this.comboBoxTable.TabIndex = 1;
+            this.comboBoxTable.SelectedIndexChanged += new System.EventHandler(this.comboBoxTable_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выберите таблицу для отображения:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 434);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxTable);
+            this.Controls.Add(this.dataGridViewData);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewData;
+        private System.Windows.Forms.ComboBox comboBoxTable;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Sales/FormMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Sales
+{
+    public partial class FormMain : Form
+    {
+        private readonly string _connectionString;
+        private readonly string _providerName;
+        private readonly DbProviderFactory _providerFactory;
+
+        public FormMain()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["Sales.Properties.Settings.SalesConnectionString"].ConnectionString;
+            _providerName = ConfigurationManager.ConnectionStrings["Sales.Properties.Settings.SalesConnectionString"].ProviderName;
+            _providerFactory = DbProviderFactories.GetFactory(_providerName);
+
+            InitializeComponent();
+            AddData();
+        }
+
+        private void AddData()
+        {
+            comboBoxTable.Items.Add("Sellers");
+            comboBoxTable.Items.Add("Buyers");
+            comboBoxTable.Items.Add("Sales");
+        }
+
+        private void comboBoxTable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (var connection = _providerFactory.CreateConnection())
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    connection.ConnectionString = _connectionString;
+                    connection.Open();
+
+                    DbDataAdapter Adapter = _providerFactory.CreateDataAdapter();
+                    command.CommandText = "Select * from " + comboBoxTable.Text;
+                    Adapter.SelectCommand = command;
+
+                    DataSet dataSet = new DataSet();
+                    dataGridViewData.DataSource = null;
+                    Adapter.Fill(dataSet);
+                    dataGridViewData.DataSource = dataSet.Tables[0];
+                }
+            }
+
+        }
+    }
+}
